1b37ab565b6ec16f9bf4969dd5ea4910b30b0326
Matheus Guimarães <91638850+guimaraesmthz@users.noreply.github.com>
2024-03-16 23:40:54 -0300

    Delete TargetSistemasChallenge directory

--- TargetSistemasChallenge/ConversorString.js
+++
@@ -1,9 +0,0 @@
-function converterString(string){
-    stringAux = "";
-    for(i = string.length-1; i >=0; i--){
-        stringAux = stringAux + string[i];
-    };
-    console.log(stringAux);
-};
-
-converterString("Matheus Nunes");
